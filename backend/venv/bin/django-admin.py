e14df57ad39c0724e5dd40483ba1d6e3fee9509e
dev-tam <tamjid.mdh@gmail.com>
2018-03-29 22:33:53 +0600

    first commit

---
+++ backend/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jtam/Development/Projects/Python/Django/todo_api/backend/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
